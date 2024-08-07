7288a4ca4ddaf154da215ddb05bd1e145bdb2bd0
Corey Kovalik <coreykovalik@gmail.com>
2024-08-07 17:18:58 -0400

    style: set fixed width/height for kraken

--- src/pages/KrakenPage/index.tsx
+++ src/pages/KrakenPage/index.tsx
@@ -3,12 +3,24 @@ import KrakenContainer from './containers/KrakenContainer';
 import MockNzxtPlugin from 'plugins/MockNzxtPlugin';
 
 const KrakenPage = () => {
+  const krakenWidth = window.nzxt?.v1?.width;
+  const krakenHeight = window.nzxt?.v1?.height;
   return (
     <>
     <NzxtPlugin />
     {import.meta.env.DEV && <MockNzxtPlugin />}
-      <div style={{ fontSize: "2em", lineHeight: 1.1, fontWeight: "bold" }}>NZTAGON</div>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      width: krakenWidth,
+      height: krakenHeight,
+      // maxHeight: "100vh",
+    }}>
+      <span style={{ fontSize: "2em", lineHeight: 1.1, fontWeight: "bold" }}>NZTAGON</span>
       <KrakenContainer />
+    </div>
     </>
   );
 }
