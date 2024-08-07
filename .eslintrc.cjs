539172df132b83e7cc3744386a689c83269209ab
Corey Kovalik <coreykovalik@gmail.com>
2024-08-07 00:35:04 -0400

    chore: adds eslint rules for variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,11 @@ module.exports = {
       { allowConstantExport: true },
     ],
     'react/react-in-jsx-scope': 'off', // Disable rule for missing import React in scope
+    "prefer-const": ["warn"],
+    // Allows unused variable names prefixed with underscore, such as: "_unusedVariable"
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }
+    ],
   },
 }
