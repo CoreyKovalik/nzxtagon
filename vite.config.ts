0f8ae11d7f5b9be1c5087af1a94f883a68cbf2be
Corey Kovalik <coreykovalik@gmail.com>
2024-08-07 16:12:49 -0400

    build: remove packageJson import to fix production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import packageJson from './package.json';
 
 /** @todo Installing types would fix this -- but I want to avoid exposing node types in the majority frontend code.*/
 // @ts-expect-error: process.env exists in the context of vite config.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const nodeProcess = (process as any);
 const isProd = nodeProcess.env === "production";
-const repositoryName = packageJson.name;
+const repositoryName = "nzxtagon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
