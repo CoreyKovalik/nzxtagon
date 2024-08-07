a1fc1e9de5b09a451ce256c07485e442f18bd446
Corey Kovalik <coreykovalik@gmail.com>
2024-08-07 16:12:04 -0400

    build: setup for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import packageJson from './package.json';
+
+/** @todo Installing types would fix this -- but I want to avoid exposing node types in the majority frontend code.*/
+// @ts-expect-error: process.env exists in the context of vite config.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const nodeProcess = (process as any);
+const isProd = nodeProcess.env === "production";
+const repositoryName = packageJson.name;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: isProd ? `/${repositoryName}/` : undefined, // necessary for gh-pages
 })
