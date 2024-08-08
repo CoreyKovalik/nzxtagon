41ce883f184c9b9f1f9d6038f7037c2466b76add
Corey Kovalik <coreykovalik@gmail.com>
2024-08-07 20:05:37 -0400

    style: center configuration page

--- src/pages/ConfigurationPage/index.tsx
+++ src/pages/ConfigurationPage/index.tsx
@@ -1,9 +1,9 @@
 const ConfigurationPage = () => {
   return (
-    <>
-    <h1>NZTAGON</h1>
-    <p className="read-the-docs">ConfigurationPage</p>
-    </>
+    <div style={{ textAlign: "center" }}>
+      <h1>NZTAGON</h1>
+      <p>ConfigurationPage</p>
+    </div>
   );
 }
 
